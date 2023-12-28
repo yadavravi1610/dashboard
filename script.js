e4cee456455c90dbbfc3c234ede6b3169bf2d8ae
Ravinder <108238004+yadavravi1610@users.noreply.github.com>
2023-12-28 15:37:33 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var header = document.getElementById('header');
+var menu = document.getElementById('menu');
+var close = document.getElementById('close');
+
+menu.addEventListener('click', ()=>{
+    header.style.display = "block";
+    menu.style.display = "none";
+})
+
