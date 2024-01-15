1f23f2f7ea955b4e0a281a40fe87c552c82b3b3a
Ravinder <108238004+yadavravi1610@users.noreply.github.com>
2024-01-15 11:09:47 +0530

    Updated css and adding a js function for the path

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 var header = document.getElementById('header');
 var menu = document.getElementById('menu');
 var close = document.getElementById('close');
+var navLinks = document.querySelectorAll('.nav-link');
+
+function highlightCurrentPage() {
+    var currentPath = window.location.pathname;
+    navLinks.forEach(link => {
+        var linkPath = link.querySelector('a').getAttribute('href');
+        if (linkPath.endsWith(currentPath)) {
+            console.log("Hello");
+            link.classList.add('highlight');
+        } else {
+            link.classList.remove('highlight');
+        }
+    });
+}
+
+
+highlightCurrentPage();
 
 menu.addEventListener('click', ()=>{
     header.style.display = "block";
